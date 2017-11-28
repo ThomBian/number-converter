9c81c707fa2c030b4cd752766f6035d92d8856ec
Thomas <thomas.bianchini1993@gmail.com>
2017-11-28 23:11:19 +0100

    [SSE] SSE done

--- public/index.js
+++ public/index.js
@@ -9,25 +9,51 @@ function convert() {
     if (!toConvert) {
         display(errorField, 'Error : Value is empty');
     } else {
-        get(toConvert);   
+        post(toConvert);   
     }
 }
 
 function display(field, message) {
-    field.innerHTML = message;
+    field.innerHTML += message;
 }
 
-function get (value) {
+function post (value) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4) {
             if (this.status == 200) {
-                const data = JSON.parse(this.responseText);
-                data.success ? 
-                    display(successField, `Result : ${data.result}`) : display(errorField, `Error : ${data.e}`);
+                successField.innerHTML += 'Request has been received';
             }
         }
     };
     xhttp.open("POST", `http://localhost:3000/api/${value}`, true);
     xhttp.send(null);
 }
+
+if (!!window.EventSource) {
+    var source = new EventSource('/sub')
+
+    source.addEventListener('message', function(e) {
+      const data = JSON.parse(e.data)
+      if (data.success) {
+        display(successField, ` // Result : ${data.result}`);
+      } else {
+        display(errorField, `Error : ${data.e}`);
+      }
+    }, false)
+
+    source.addEventListener('open', function(e) {
+        console.log('Opened');
+    }, false)
+
+    source.addEventListener('error', function(e) {
+      if (e.target.readyState == EventSource.CLOSED) {
+        console.log("Disconnected")
+      }
+      else if (e.target.readyState == EventSource.CONNECTING) {
+        console.log("Connecting...")
+      }
+    }, false)
+  } else {
+    console.log("Your browser doesn't support SSE")
+  }
--- server.js
+++ server.js
@@ -1,27 +1,44 @@
 const express = require('express');
 const path = require('path');
 
-const converter = require('./converter');
+const converter = require('./modules/converter');
 
 const app = express();
 app.use(express.static('public'));
 
+let connection;
+
 function convertRoman(req, res) {
     const value = req.params.value;
-    console.log('youhou', value);
+    res.sendStatus(200);
     return converter.toRoman(value)
         .then(converted => {
-            res.send({
+            sendValue({
                 success: true,
                 result: converted
             });
         })
         .catch(e => {
-            res.send({
+            sendValue({
                 success: false,
                 e: e.message
             });
         });
+}
+
+function register(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    });
+    connection = res;
+}
+
+function sendValue (data) {
+    if (connection) {
+        connection.write(`data: ${JSON.stringify(data)} \n\n`);
+    }
 }
 
 function home(req, res) {
@@ -30,6 +47,7 @@ function home(req, res) {
 
 app
     .post('/api/:value', convertRoman)
+    .get('/sub', register)
     .get('/', home);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
